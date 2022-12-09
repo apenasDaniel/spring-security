7fde37cf5a9b42952e8f3c634ff45ec3754c469c
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-12-09 10:59:17 -0300

    quando o usuário tentar registrar-se no sistema, os dados serão salvos no banco de dados e o usuário sera desativado na inicialização

---
+++ src/main/java/com/example/springsecurityclient/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.springsecurityclient.service;
+
+import com.example.springsecurityclient.entity.User;
+import com.example.springsecurityclient.model.UserModel;
+
+public interface UserService {
+    User registerUser(UserModel userModel);
+}
